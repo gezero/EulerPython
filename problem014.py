1637e1c3f75ae84b587cd720aa876ae2ba4f48f7
Jiri Peinlich <jiri.peinlich@gmail.com>
2014-07-06 09:30:32 +0200

    pollished problems 14,15

--- problem014.py
+++ problem014.py
@@ -1,22 +1,37 @@
-stop=1000001
-memory =[0]*stop
-memory[1]=1
+# The following iterative sequence is defined for the set of positive integers:
+# 
+# n → n/2 (n is even)
+# n → 3n + 1 (n is odd)
+# 
+# Using the rule above and starting with 13, we generate the following
+# sequence:
+# 
+# 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+# It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+# 
+# Which starting number, under one million, produces the longest chain?
+# 
+# NOTE: Once the chain starts the terms are allowed to go above one million.
+
+memory_limit = 99999999
+memory = [0] * memory_limit
+memory[1] = 1
 
 def algorithm(n):
-	if n>stop:
-		return -1 	
-	if memory[n]!=0:		
-		return memory[n] 
-	memory[n] = 1+(algorithm(n//2) if n%2 ==0 else algorithm(n*3+1))	
+	if n >= memory_limit:
+		return 1 + (algorithm(n // 2) if n % 2 == 0 else algorithm(n * 3 + 1)) 	
+	if memory[n] == 0:		
+		memory[n] = 1 + (algorithm(n // 2) if n % 2 == 0 else algorithm(n * 3 + 1))	
 	return memory[n]
 	
 
-max = 0
+print(algorithm(13))
+
+current_max = 0
 n=0
-for x in range(1,100000):
+for x in range(1, 1000000):
 	a = algorithm(x)
-	if a>max:
-		max = a
-		n=x
-print (max)
-print (n)
+	if a > current_max:
+		current_max = a
+		n = x
+print (current_max, n)
--- problem015.py
+++ problem015.py
@@ -1,10 +1,21 @@
-matrix_size =20
-matrix = [[0 for i in range(0,matrix_size)] for i in range(0,matrix_size)]
-for i in range(0,matrix_size):
-	matrix[0][i]=1
-	matrix[i][0]=1
-for i in range(1,matrix_size):
-	for j in range(1,matrix_size):
-		matrix[i][j]=matrix[i][j-1]+matrix[i-1][j]
+# link: https://projecteuler.net/problem=15
+
+# Starting in the top left corner of a 2×2 grid, and only being able to move to
+# the right and down, there are exactly 6 routes to the bottom right corner.
+
+################################################################################
+######## Picture missing ##### check the pickture using the link ###############
+################################################################################
+
+# How many such routes are there through a 20×20 grid?
+
+matrix_size = 21
+matrix = [[0 for i in range(0, matrix_size)] for i in range(0, matrix_size)]
+for i in range(0, matrix_size):
+    matrix[0][i] = 1
+    matrix[i][0] = 1
+for i in range(1, matrix_size):
+    for j in range(1, matrix_size):
+        matrix[i][j] = matrix[i][j-1] + matrix[i-1][j]
 print(matrix)
 print(matrix[matrix_size-1][matrix_size-1])
