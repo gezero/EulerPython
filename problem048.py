60b33a15d4b76df2d7e43afd93f8531ff229c710
Jiri Peinlich <jiri.peinlich@gmail.com>
2014-08-04 08:24:21 +0200

    problem 48

--- problem048.py
+++ problem048.py
@@ -3,3 +3,18 @@
 # The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
 
 # Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+
+
+mod = 10**10
+def last10DigitsNtoN(n):
+    total = 1
+    for i in range(n):
+        total = (total *n) % mod
+    return total
+
+
+total = 0
+for i in range(1,1001):
+    total += last10DigitsNtoN(i)
+print(total)
+print(total % mod)
